78ce7903c91a3b017b3352277c9292cccc734b52
Bharat <hibharatj@gmail.com>
2026-02-06 11:32:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,62 +1,70 @@
-//your JS code here. If required.
-var app = document.getElementById('app');
 var video = document.querySelector('.vid-container video');
 var audio = document.querySelector('.player-container audio');
 var playBtn = document.querySelector('.play');
 var timeDisplay = document.querySelector('.time-display');
 var soundButtons = document.querySelectorAll('.sound-picker button');
-var timeButtons = document.querySelectorAll('#time-select button');
+var timeButtons = document.querySelectorAll('.time-select button');
 
-var duration = 600; // default 10 minutes
+var duration = 600;
+var currentTime = duration;
+var isPlaying = false;
+var timer = null;
+
+// Update time display
+function updateTime() {
+  var minutes = Math.floor(currentTime / 60);
+  var seconds = currentTime % 60;
+  timeDisplay.innerHTML = minutes + ':' + seconds;
+}
 
 // Play / Pause
 playBtn.addEventListener('click', function () {
-  if (audio.paused) {
+  if (!isPlaying) {
+    isPlaying = true;
+    playBtn.innerHTML = 'Pause';
     audio.play();
     video.play();
-    playBtn.innerHTML = 'Pause';
+
+    timer = setInterval(function () {
+      if (currentTime > 0) {
+        currentTime--;
+        updateTime();
+      } else {
+        clearInterval(timer);
+        audio.pause();
+        video.pause();
+        isPlaying = false;
+        playBtn.innerHTML = 'Play';
+      }
+    }, 1000);
+
   } else {
+    isPlaying = false;
+    playBtn.innerHTML = 'Play';
     audio.pause();
     video.pause();
-    playBtn.innerHTML = 'Play';
+    clearInterval(timer);
   }
 });
 
-// Change Sound + Video
+// Change sound + video
 for (var i = 0; i < soundButtons.length; i++) {
   soundButtons[i].addEventListener('click', function () {
     audio.src = this.getAttribute('data-sound');
     video.src = this.getAttribute('data-video');
-    audio.play();
-    video.play();
-    playBtn.innerHTML = 'Pause';
+
+    if (isPlaying) {
+      audio.play();
+      video.play();
+    }
   });
 }
 
-// Time Selection
+// Time selection
 for (var j = 0; j < timeButtons.length; j++) {
   timeButtons[j].addEventListener('click', function () {
     duration = parseInt(this.getAttribute('data-time'));
-    updateTime(duration);
+    currentTime = duration;
+    updateTime();
   });
-}
-
-// Update countdown
-audio.addEventListener('timeupdate', function () {
-  var remaining = duration - audio.currentTime;
-
-  if (remaining <= 0) {
-    audio.pause();
-    video.pause();
-    audio.currentTime = 0;
-    playBtn.innerHTML = 'Play';
-  }
-
-  updateTime(remaining);
-});
-
-function updateTime(time) {
-  var minutes = Math.floor(time / 60);
-  var seconds = Math.floor(time % 60);
-  timeDisplay.innerHTML = minutes + ':' + seconds;
 }
